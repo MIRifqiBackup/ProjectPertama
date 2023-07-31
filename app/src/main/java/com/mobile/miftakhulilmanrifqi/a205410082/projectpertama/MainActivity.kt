9899b32aebf305c0005957330b857471717ae300
Miftakhul-Ilman-Rifqi <bozzclax@gmail.com>
2023-07-31 23:55:25 +0700

    first commit

---
+++ app/src/main/java/com/mobile/miftakhulilmanrifqi/a205410082/projectpertama/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mobile.miftakhulilmanrifqi.a205410082.projectpertama
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
